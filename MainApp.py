cd434706754089e05ff9e903a563f6634414fa9b
midas <midas.stienaers@hotmail.com>
2017-08-08 20:50:31 +0200

    multiple elements

--- MainApp.py
+++ MainApp.py
@@ -3,15 +3,19 @@ kivy.require('1.0.6')
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 
 class MyApp(App):
 
     def build(self):
         layout = BoxLayout(padding=10)
-        button = Button(text='My first button')
+        button = Button(text='My first button', background_color=(1,0,0,1))
         layout.add_widget(button)
-        return Label(text='Hello world, this works ... awesome')
+		label = Label(text='Hello world, this works ... awesome')
+		layout.add_widget(label)
+        return layout
 
 
 if __name__ == '__main__':
