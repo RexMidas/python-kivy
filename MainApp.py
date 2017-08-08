b630ec90b5949f0df0561e8c59efeee22cb5c34a
midas <midas.stienaers@hotmail.com>
2017-08-08 20:55:43 +0200

    scatter added

--- MainApp.py
+++ MainApp.py
@@ -5,16 +5,19 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scatter import Scatter
 
 
 class MyApp(App):
 
     def build(self):
         layout = BoxLayout(padding=10)
+        scatter = Scatter()
         button = Button(text='My first button', background_color=(1,0,0,1))
         layout.add_widget(button)
         label = Label(text='Hello world, this works ... awesome')
-        layout.add_widget(label)
+        scatter.add_widget(label)
+        layout.add_widget(scatter)
         return layout
 
 
